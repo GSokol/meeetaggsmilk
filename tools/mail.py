a5c8698debe9b8c8bb54d6b75ff17bb65cc59b1a
sokol <g.sokol99@g-sokol.info>
2015-03-22 15:34:55 +0300

    trying to send mails

--- tools/mail.py
+++ tools/mail.py
@@ -1,11 +1,22 @@
 # -*- coding: utf-8 -*-
-from django.core.mail import send_mail
+
+from os.path import join
+from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
+from meataggsmilk.settings import STATICFILES_DIRS, MEDIA_ROOT
+from setts.models import ImageModel
+
 def send(title, message, template, context, to):
+  context['logo'] = ImageModel.objects.get(imgType = LOGO)[0].image.filename
   if isinstance(to, basestring):
-    send_mail(title, message, u'order@xn--80aredccldbby6d7fc.xn--p1ai',
-        (to,), html_message=render_to_string('mail/' + template, context))
+    message = EmailMultiAlternatives(title, message, u'order@xn--80aredccldbby6d7fc.xn--p1ai', (to,))
+    message.attach_alternative(render_to_string('mail/' + template, context))
+    message.attach_file(join(STATICFILES_DIRS[0], 'img', 'background1.png'))
+    message.attach_file(join(MEDIA_ROOT, context['logo']))
+    message.send()
+#    send_mail(title, message, u'order@xn--80aredccldbby6d7fc.xn--p1ai',
+#        (to,), html_message=render_to_string('mail/' + template, context))
   else:
     for addr in to:
       send(title, message, template, context, addr)
